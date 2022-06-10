3b7ce0cae07229306f060b96095015028b044444
Andres Seivane <aseivane@edrans.com>
2022-06-09 23:53:06 -0300

    S3 backend

--- backend.tf
+++ backend.tf
@@ -6,12 +6,12 @@ terraform {
       version = "~> 4.18.0"
     }
   }
-  required_version = ">= 0.13"/*
+  required_version = ">= 0.13"
   backend "s3" {
     bucket        = "terraform-onboarding-aseivane-tfstate"
     key           = "terraform.tfstate"
     region        = "us-east-1"
     encrypt       = true
     kms_key_id    = "alias/aws/s3"
-  }*/
+  }
 }
